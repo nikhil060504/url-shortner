c56ebc0816fbb30f9d5d6210fce6b483d9d7cf61
nikhil060504 <patidarnikhil314@gmail.com>
2025-06-05 15:00:33 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -32,7 +32,9 @@ app.get("/:id", redirectFromShortUrl);
 
 app.use(errorHandler);
 
-app.listen(5000, () => {
-  connectDB();
-  console.log("Server is running onport http://localhost:5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+  connectDB()
 });
